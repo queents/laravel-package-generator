d7e9bfa487634a3deb208afb1d6263fd8b900c06
Fady Mondy <EngFadyMondy@gmail.com>
2023-01-11 13:32:34 +0200

    first commit 🔥

--- src/Console/LaravelPackageGenerator.php
+++ src/Console/LaravelPackageGenerator.php
@@ -4,13 +4,18 @@ namespace Queents\LaravelPackageGenerator\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Queents\ConsoleHelpers\Traits\HandleFiles;
+use Queents\ConsoleHelpers\Traits\HandleStub;
 use Queents\ConsoleHelpers\Traits\RunCommand;
 
 class LaravelPackageGenerator extends Command
 {
     use RunCommand;
     use HandleFiles;
+    use HandleStub;
+
+    private string $stubPath;
 
     /**
      * The name and signature of the console command.
@@ -29,7 +34,8 @@ class LaravelPackageGenerator extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->publish = __DIR__ .'../../publish/';
+        $this->publish = __DIR__ .'/../../publish/';
+        $this->stubPath = __DIR__ .'/../../stubs/';
     }
 
 
@@ -43,19 +49,34 @@ class LaravelPackageGenerator extends Command
         $packageName = null;
         while(empty($packageName)){
             $packageName = $this->ask("Enter your package name");
+            if(is_numeric($packageName[0])){
+                $this->error("Package name can't start with a number");
+                $packageName = null;
+            }
         }
+        $packageString = Str::of($packageName);
+
         $packageVendor = null;
         while(empty($packageVendor)){
             $packageVendor =  $this->ask("Enter your package vendor name");
+            if(is_numeric($packageVendor[0])){
+                $this->error("Vendor name can't start with a number");
+                $packageVendor = null;
+            }
         }
 
-        $packageDescription = $this->ask("Enter your package description");
-        $packageAuthor = $this->ask("Enter your package author");
-        $packageAuthorEmail = $this->ask("Enter your package author email");
-        $packageConfig = $this->ask("Has Config file? (yes/no)");
-        $packageRoute = $this->ask("Has Routes ? (yes/no)");
-        $packageView = $this->ask("Has Views ? (yes/no)");
-        $packageMigration = $this->ask("Has Migrations? (yes/no)");
+        $packageVendorString = Str::of($packageVendor);
+
+        //Package Meta
+        $packageDescription = $this->ask("Enter your package description", "your package description will go here");
+        $packageAuthor = $this->ask("Enter your package author", "Queen Tech");
+        $packageAuthorEmail = $this->ask("Enter your package author email", "git@queentechsoltions.net");
+
+        //Check Options
+        $packageConfig = $this->ask("Has Config file? (yes/no)", "yes")?: "yes";
+        $packageRoute = $this->ask("Has Routes? (yes/no)", "yes")?: "yes";
+        $packageView = $this->ask("Has Views? (yes/no)", "yes")?: "yes";
+        $packageMigration = $this->ask("Has Migrations? (yes/no)", "yes")?: "yes";
 
         $this->info('Generating package boilerplate...');
 
@@ -64,19 +85,166 @@ class LaravelPackageGenerator extends Command
             File::makeDirectory(base_path('packages'));
         }
 
+        $packageVendorPath = $packageVendorString
+            ->replace(' ', '-')
+            ->replace('_', '-')
+            ->lower()
+            ->toString();
+
         //Create vendor directory
-        if(!File::exists(base_path('packages/'.$packageVendor))){
-            File::makeDirectory(base_path('packages/'.$packageVendor));
+        if(!File::exists(base_path('packages') . "/" .$packageVendorPath)){
+            File::makeDirectory(base_path('packages') . "/" .$packageVendorPath);
         }
 
+        $packageNamePath = $packageString
+            ->replace(' ', '-')
+            ->replace('_', '-')
+            ->lower()
+            ->toString();
+
+        //Create package directory
+        if(!File::exists(base_path('packages') . "/" .$packageVendor . "/" . $packageNamePath)){
+            File::makeDirectory(base_path('packages') . "/" .$packageVendor . "/" . $packageNamePath);
+        }
+
+        $packagePath = base_path('packages') . "/" .$packageVendorPath . "/" . $packageNamePath;
+
         //Build a package inside vendor directory
-        $this->handelFile('config', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('database', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('resources', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('routes', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('src', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('tests', base_path('packages'.$packageVendor), 'folder');
-        $this->handelFile('LICENSE.md', base_path('packages'.$packageVendor));
+        $packageConfig !== 'yes' ? null : $this->handelFile('config', $packagePath. "/config", 'folder');
+        $packageMigration !== 'yes' ? null : $this->handelFile('database', $packagePath. "/database", 'folder');
+        $packageView !== 'yes' ? null : $this->handelFile('resources', $packagePath. "/resources", 'folder');
+        $packageRoute !== 'yes' ? null : $this->handelFile('routes', $packagePath. "/routes", 'folder');
+        $this->handelFile('src', $packagePath. "/src", 'folder');
+        $this->handelFile('tests', $packagePath. "/tests", 'folder');
+        $this->handelFile('LICENSE.md', $packagePath. "/LICENSE.md");
+        $this->handelFile('CHANGELOG.md', $packagePath. "/CHANGELOG.md");
+        $this->handelFile('SECURITY.md', $packagePath. "/SECURITY.md");
+
+
+        $vendorNamespace = Str::of($packageVendorPath)->camel()->ucfirst()->toString();
+        $packageNamespace = Str::of($packageNamePath)->camel()->ucfirst()->toString();
+        $packageProviderName = Str::of($packageNamePath)->camel()->ucfirst()->toString() . "ServiceProvider";
+        $fullNameSpace = $vendorNamespace . "\\". $packageNamespace;
+
+        //Build Stubs Files
+        $commandClassName = $packageString->studly()->append('Install')->toString();
+        $this->generateStubs(
+            $this->stubPath . 'command.stub',
+            $packagePath . '/src/Console/'. $commandClassName . ".php",
+            [
+                "namespace" => $fullNameSpace,
+                "name" => $commandClassName,
+                "command" => $packageNamePath,
+                "packageName" => Str::of($packageNamePath)->camel()->toString(),
+                "provider" => $packageProviderName
+            ],
+            [
+                $packagePath . '/src/Console/'
+            ]
+        );
+
+        //Build Provider Register Methods
+        $register = collect([]);
+        $register->push('//Register generate command');
+        $register->push('        $this->commands([');
+        $register->push('           '."\\".$fullNameSpace."\\Console\\".$commandClassName.'::class,');
+        $register->push('        ]);');
+        $register->push(" ");
+        if($packageConfig === 'yes'){
+            $register->push('        //Register Config file');
+            $register->push('        $this->mergeConfigFrom(__DIR__.\'/../config/'.$packageNamePath.'.php\', \''.$packageNamePath.'\');');
+            $register->push(" ");
+            $register->push('        //Publish Config');
+            $register->push('        $this->publishes([');
+            $register->push('           __DIR__.\'/../config/'.$packageNamePath.'.php\' => config_path(\''.$packageNamePath.'.php\'),');
+            $register->push('        ], \'config\');');
+            $register->push(" ");
+        }
+        if($packageMigration === 'yes'){
+            $register->push('        //Register Migrations');
+            $register->push('        $this->loadMigrationsFrom(__DIR__.\'/../database/migrations\');');
+            $register->push(" ");
+            $register->push('        //Publish Migrations');
+            $register->push('        $this->publishes([');
+            $register->push('           __DIR__.\'/../database/migrations\' => database_path(\'migrations\'),');
+            $register->push('        ], \'migrations\');');
+        }
+        if($packageView === 'yes'){
+            $register->push('        //Register views');
+            $register->push('        $this->loadViewsFrom(__DIR__.\'/../resources/views\', \''.$packageNamePath.'\');');
+            $register->push(" ");
+            $register->push('        //Publish Views');
+            $register->push('        $this->publishes([');
+            $register->push('           __DIR__.\'/../resources/views\' => resource_path(\'views/vendor/'.$packageNamePath.'\'),');
+            $register->push('        ], \'views\');');
+            $register->push(" ");
+            $register->push('        //Register Langs');
+            $register->push('        $this->loadTranslationsFrom(__DIR__.\'/../resources/lang\', \''.$packageNamePath.'\');');
+            $register->push(" ");
+            $register->push('        //Publish Lang');
+            $register->push('        $this->publishes([');
+            $register->push('           __DIR__.\'/../resources/lang\' => resource_path(\'lang/vendor/'.$packageNamePath.'\'),');
+            $register->push('        ], \'lang\');');
+            $register->push(" ");
+        }
+        if($packageRoute === 'yes') {
+            $register->push('        //Register Routes');
+            $register->push('        $this->loadRoutesFrom(__DIR__.\'/../routes/web.php\');');
+            $register->push(" ");
+        }
+
+        //Generate Provider File
+        $this->generateStubs(
+            $this->stubPath . 'provider.stub',
+            $packagePath . '/src/'. $packageProviderName . ".php",
+            [
+                "namespace" => $fullNameSpace,
+                "name" => $packageProviderName,
+                "register" => $register->implode("\n")
+            ],
+            [
+                $packagePath . '/src'
+            ]
+        );
+
+        //Generate Composer.json file
+        $this->generateStubs(
+            $this->stubPath . 'composer.stub',
+            $packagePath . '/composer.json',
+            [
+                "vendor" => $packageVendorPath,
+                "package" => $packageNamePath,
+                "description" => $packageDescription,
+                "namespace" => $vendorNamespace ."\\\\". $packageNamespace,
+                "provider" => $packageProviderName,
+                "author" => $packageAuthor,
+                "email" => $packageAuthorEmail,
+            ],
+            [
+                $packagePath
+            ]
+        );
+
+        //Generate Readme.md file
+        $this->generateStubs(
+            $this->stubPath . 'readme.stub',
+            $packagePath . '/README.md',
+            [
+                "name" => Str::of($packageNamePath)->replace('-', ' ')->ucfirst()->toString(),
+                "vendor" => $packageVendorPath,
+                "command" => $packageNamePath,
+                "package" => $packageNamePath,
+                "description" => $packageDescription,
+                "author" => $packageAuthor,
+                "email" => $packageAuthorEmail,
+                "vendorName" => $vendorNamespace
+            ],
+            [
+                $packagePath
+            ]
+        );
+
+
 
 
         $this->info('Package boilerplate generated successfully');
